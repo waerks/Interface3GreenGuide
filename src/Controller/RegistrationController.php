6703e417296975984809cb4e9ca8e3a521162557
Anaïs <anais.aerts@gmail.com>
2024-09-26 17:14:07 +0200

    Inscription fonctionelle

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -5,11 +5,13 @@ namespace App\Controller;
 use App\Entity\User;
 use App\Form\RegistrationFormType;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class RegistrationController extends AbstractController
 {
@@ -23,21 +25,41 @@ class RegistrationController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             /** @var string $plainPassword */
             $plainPassword = $form->get('plainPassword')->getData();
-
-            // encode the plain password
+        
+            // encode le mot de passe
             $user->setPassword($userPasswordHasher->hashPassword($user, $plainPassword));
-
-            // set default role
+        
+            // set le rôle par défaut
             $user->setRoles(['ROLE_USER']);
-
+        
+            // Gestion de l'upload de l'avatar
+            $avatarFile = $form->get('avatar')->getData();
+            if ($avatarFile) {
+                // Génère un nom unique pour l'avatar
+                $newFileName = uniqid().'.'.$avatarFile->guessExtension();
+        
+                // Déplace le fichier dans le répertoire de destination
+                try {
+                    $avatarFile->move(
+                        $this->getParameter('kernel.project_dir') . '/public/build/images/avatars', // ou le paramètre que tu as défini
+                        $newFileName
+                    );
+                    // Met à jour l'utilisateur avec le nouveau nom de fichier
+                    $user->setAvatar($newFileName);
+                } catch (FileException $e) {
+                    // Gérer l'exception en cas d'échec de déplacement
+                    $this->addFlash('error', 'L\'upload de l\'avatar a échoué.');
+                    return $this->redirectToRoute('inscription');
+                }
+            }
+        
+            // Persiste l'utilisateur dans la base de données
             $entityManager->persist($user);
             $entityManager->flush();
-
-            // do anything else you need here, like send an email
-
-
+        
             return $this->redirectToRoute('accueil');
         }
+        
 
         return $this->render('registration/register.html.twig', [
             'registrationForm' => $form,
--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -5,9 +5,11 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\File;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
@@ -21,7 +23,23 @@ class RegistrationFormType extends AbstractType
             ->add('nom')
             ->add('prenom')
             ->add('pseudo')
-            ->add('avatar')
+            ->add('avatar', FileType::class, [
+                'label' => 'Avatar (Image file)',
+
+                'mapped' => false,
+
+                'constraints' => [
+                    new File([
+                        'maxSize' => '1024k',
+                        'mimeTypes' => [
+                            'image/jpeg',
+                            'image/jpg',
+                            'image/png',
+                        ],
+                        'mimeTypesMessage' => 'Veuillez télécharger une image valide (JPEG ou JPG ou PNG)',
+                    ])
+                ]
+            ])
             // ->add('agreeTerms', CheckboxType::class, [
             //     'mapped' => false,
             //     'constraints' => [
