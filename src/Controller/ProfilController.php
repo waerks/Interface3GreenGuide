e775b1cc3618782d73e8fa4f38d47050e68f3180
Anaïs <a.aerts@interface3.be>
2024-10-01 14:33:34 +0200

    Affichage recette user profil

--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Recette;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
@@ -9,10 +11,23 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ProfilController extends AbstractController
 {
+    private $doctrine;
+
+    public function __construct(ManagerRegistry $doctrine)
+    {
+        $this->doctrine = $doctrine;
+    }
     #[IsGranted('ROLE_USER')]
     #[Route('/profil', name: 'profil')]
     public function index(): Response
     {
-        return $this->render('profil/profil.html.twig');
+        $user = $this->getUser();
+        $recettes = $this->doctrine->getRepository(Recette::class)->findBy(['user' => $user]);
+
+        $vars = [
+            'recettes' => $recettes
+        ];
+
+        return $this->render('profil/profil.html.twig', $vars);
     }
 }
